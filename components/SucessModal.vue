d3a35001d3971ec01ef50c98c345899b7ceff6fc
Victor Hugo de Souza Pereira <83098581+Lkt226@users.noreply.github.com>
2021-11-10 09:14:29 -0300

    lkt226

---
+++ components/SucessModal.vue
@@ -0,0 +1,87 @@
+<template>
+  <section id="sucess-modal">
+    <div class="card">
+      <NuxtLink to="/"><p class="close">x</p></NuxtLink>
+      <div class="card-header">
+        <Icon name="check-sucess" size="5rem" color="var(--color-green)" />
+      </div>
+      <div class="card-body">
+        <h3>Seu cadastro foi concluido com sucesso!</h3>
+      </div>
+      <div class="card-footer" @click="goToHome">
+        <Icon name="left-arrow" size="1rem" color="var(--color-purple)"/>
+        <p>VOLTAR PARA HOME</p>
+      </div>
+    </div>
+
+  </section>
+</template>
+
+<script>
+export default {
+  methods:{
+    goToHome(){
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  #sucess-modal {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    z-index: 9999;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .card{
+      align-items: flex-end;
+    .close{
+      font-size: 1.2rem;
+      color: var(--color-black);
+      font-weight: normal;
+      margin-right: 0.5rem;
+      cursor: pointer;
+    }
+    .card-header{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+      background: none;
+      border: none;
+    }
+    .card-body{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+    }
+    .card-footer{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      cursor: pointer;
+
+      width: 100%;
+      padding-top: 0;
+      padding-bottom: 2.5rem;
+
+      background: none;
+      border: none;
+      p{
+        color: var(--color-purple);
+        padding-left: 0.3rem;
+        font-weight: bold;
+      }
+    }
+  }
+
+</style>
--- pages/checkout.vue
+++ pages/checkout.vue
@@ -1,195 +1,285 @@
 <template>
   <div class="body">
-    <Header/>
+    <Header />
     <main>
-      <form action="" method="post" class="offsetBoard">
+      <form action="" name="checkout" method="post" class="offsetBoard" @submit="handleSubmit">
         <label id="name">
-          <p>Nome*</p> <input type="text" name="name">
+          <p>Nome*</p>
+          <input type="text" required name="name"/>
         </label>
         <label id="email">
-          <p>E-mail*</p> <input type="email" name="email">
+          <p>E-mail*</p>
+          <input type="email" required name="email" placeholder="voce@gmail.com" />
         </label>
         <label id="cpf">
-          <p>CPF*</p> <input type="password" name="cpf">
+          <p>CPF*</p>
+          <input type="text" inputmode="numeric" minlength="11" maxlength="14" placeholder="111.111.111-99"
+          pattern="^[0-9]{3}.?[0-9]{3}.?[0-9]{3}-?[0-9]{2}" :value="myCPF" @input="formatCPF" required name="cpf"/>
         </label>
         <label id="birthday">
-          <p>Data de Nascimento*</p> <input type="date" name="birthday">
+          <p>Data de Nascimento*</p>
+          <input type="date" required name="birthday" />
         </label>
         <label id="telphone">
-          <p>Telefone*</p> <input type="tel" name="telphone">
+          <p>Telefone*</p>
+          <input type="text" required name="telphone" placeholder="+55 21 98930-6866" minlength="11" maxlength="15"
+          inputmode="tel" :value="myTel" @input="formatTel"/>
         </label>
 
         <label id="cep">
-          <p>CEP*</p> <input type="text" name="cep">
+          <p>CEP*</p>
+          <input type="text" required name="cep" placeholder="24860-148" minlength="8" maxlength="9"
+          :value="myCEP" @input="formatCEP" @change="getCEP"/>
         </label>
         <label id="endress">
-          <p>Endereço*</p> <input type="text" name="endress">
+          <p>Endereço*</p>
+          <input type="text" required name="endress" :value="EndressComplete.endress" />
         </label>
         <label id="num">
-          <p>Número*</p> <input type="text" name="num">
+          <p>Número</p>
+          <input type="number" name="num" />
         </label>
         <label id="complement">
-          <p>Complemento</p> <input type="text" name="complement">
+          <p>Complemento</p>
+          <input type="text" name="complement" :value="EndressComplete.complement" />
         </label>
         <label id="bairro">
-          <p>Bairro*</p> <input type="text" name="bairro">
+          <p>Bairro*</p>
+          <input type="text" required name="bairro" :value="EndressComplete.bairro" />
         </label>
         <label id="city">
-          <p>Cidade*</p> <input type="text" name="city">
+          <p>Cidade*</p>
+          <input type="text" required name="city" :value="EndressComplete.city"/>
         </label>
         <label id="state">
-          <p>Estado*</p> <input type="text" name="state">
+          <p>Estado*</p>
+          <input type="text" required name="state" :value="EndressComplete.state" />
         </label>
 
-        <button id="button_finish" class="diferent-button" type="submit">Concluir compra</button>
+        <button id="button_finish" class="diferent-button" type="submit">
+          Concluir compra
+        </button>
       </form>
     </main>
-    <Footer class="fixed"/>
+    <Footer class="fixed" />
+    <SucessModal :style="modal === true ? '' : 'display: none;'"/>
   </div>
 </template>
 
 <script>
 export default {
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault()
+      this.modal = true
+    },
+    async getCEP(e) {
+      let cep = e.target.value
+      cep = cep.replace("-", '')
+      if (cep.length === 8) {
+        let CepEndress = await this.$axios.$get(`https://viacep.com.br/ws/${cep}/json/`)
+        if (CepEndress.erro) {
+          console.log('CEP não encontrado')
+        } else {
+           this.EndressComplete = {
+            endress: CepEndress.logradouro,
+            complement: CepEndress.complemento,
+            bairro: CepEndress.bairro,
+            city: CepEndress.localidade,
+            state: CepEndress.uf
+          }
+        }
+      }
+    },
 
-}
+    // Formats
+    formatCPF(e) {
+      let value = e.target.value
+      value = value.replace(/\D/g, '')
+      value = value.replace(/(\d{3})(\d)/, '$1.$2')
+      value = value.replace(/(\d{3})(\d)/, '$1.$2')
+      value = value.replace(/(\d{3})(\d{1,2})$/, '$1-$2')
+      this.myCPF = value
+    },
+    formatCEP(e) {
+      let value = e.target.value
+      value = value.replace(/\D/g, '')
+      value = value.replace(/(\d{5})(\d)/, '$1-$2')
+      this.myCEP = value
+    },
+    formatTel(e) {
+      let value = e.target.value
+      value = value.replace(/\D/g, '')
+      value = value.replace(/(\d{2})(\d)/, '($1) $2')
+      value = value.replace(/(\d{5})(\d)/, '$1-$2')
+      this.myTel = value
+    }
+  },
+  data() {
+    return {
+      myCPF: '',
+      myCEP: '',
+      myTel: '',
+      EndressComplete: {
+        endress: '',
+        complement: '',
+        bairro: '',
+        city: '',
+        state: ''
+      },
+      modal: false,
+    }
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  .fixed {
-    position: fixed;
-    bottom: 0;
-    width: 100%;
+.fixed {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
 
-    @media (max-width: 576px) {
-      position: relative;
-    }
+  @media (max-width: 576px) {
+    position: relative;
   }
-  .body {
-    width: 100%;
+}
+.body {
+  width: 100%;
+  height: 100%;
+}
+
+main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 80%;
+
+  @media (max-width: 576px) {
     height: 100%;
   }
+}
 
-  main {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      height: 80%;
+form {
+  display: grid;
+  grid-gap: 1rem;
+  grid-row-gap: 0.5rem;
 
-      @media (max-width: 576px) {
-        height: 100%;
-      }
-    }
+  grid-template:
+    "name name cep cep"
+    "email email endress num"
+    "cpf cpf complement bairro"
+    "birthday telphone city state"
+    "none none finish finish"/
+    1fr 1fr 1fr 1fr;
 
-
-  form{
-    display: grid;
-    grid-gap: 1rem;
-    grid-row-gap: 0.5rem;
-
+  @media (max-width: 576px) {
     grid-template:
-      "name name cep cep"
-      "email email endress num"
-      "cpf cpf complement bairro"
-      "birthday telphone city state"
-      "none none finish finish"/
-      1fr 1fr 1fr 1fr;
-
-    @media (max-width: 576px) {
-      grid-template:
-        "name name"
-        "cep cep"
-        "email email"
-        "endress num"
-        "cpf cpf"
-        "complement bairro"
-        "birthday telphone"
-        "city state"
-        "finish finish"/
-        1fr 1fr;
-    }
-
-    label{
-      display: block;
-    }
-
-    #name, #email, #cpf, #birthday, #telphone, #cep, #endress, #num, #complement, #bairro, #city, #state{
-      input{
-        width: 100%;
-        height: 2rem;
-
-        border: 1px solid var(--color-black);
-        border-radius: 2px;
-        padding: 5px;
-      }
-    }
+      "name name"
+      "cep cep"
+      "email email"
+      "endress num"
+      "cpf cpf"
+      "complement bairro"
+      "birthday telphone"
+      "city state"
+      "finish finish"/
+      1fr 1fr;
   }
 
-  form{ //Templates de aréa
-     #name{
-      grid-area: name;
-    }
-    #email{
-      grid-area: email;
-    }
-    #cpf{
-      grid-area: cpf;
-    }
-    #birthday{
-      grid-area: birthday;
-    }
-    #telphone{
-      grid-area: telphone;
-    }
-    #cep{
-      grid-area: cep;
-    }
-    #endress{
-      grid-area: endress;
-      width: 130%;
-    }
-    #num{
-      grid-area: num;
-      width: 70%;
-      justify-self: right;
-    }
-    #complement{
-      grid-area: complement;
-      width: 90%;
-    }
-    #bairro{
-      grid-area: bairro;
-      width: 110%;
-      justify-self: right;
-    }
-    #city{
-      grid-area: city;
-      width: 130%;
-    }
-    #state{
-      grid-area: state;
-      width: 70%;
-      justify-self: right;
-    }
-    #button_finish{
-      grid-area: finish;
-      width: 70%;
-      justify-self: right;
-    }
+  label {
+    display: block;
   }
 
-  .diferent-button{
-    background-color: var(--color-gray);
-    color: var(--color-black);
+  #name,
+  #email,
+  #cpf,
+  #birthday,
+  #telphone,
+  #cep,
+  #endress,
+  #num,
+  #complement,
+  #bairro,
+  #city,
+  #state {
+    input {
+      width: 100%;
+      height: 2rem;
 
-    padding: 0.5rem 1rem;
-
-    border: none;
-    border-radius: 3px;
-    cursor: pointer;
-
-    font-size: 1rem;
-    font-weight: bold;
-    background-color: var(--color-purple);
-    color: var(--color-white);
+      border: 1px solid var(--color-black);
+      border-radius: 2px;
+      padding: 5px;
+    }
   }
+}
+
+form {
+  //Templates de aréa
+  #name {
+    grid-area: name;
+  }
+  #email {
+    grid-area: email;
+  }
+  #cpf {
+    grid-area: cpf;
+  }
+  #birthday {
+    grid-area: birthday;
+  }
+  #telphone {
+    grid-area: telphone;
+  }
+  #cep {
+    grid-area: cep;
+  }
+  #endress {
+    grid-area: endress;
+    width: 130%;
+  }
+  #num {
+    grid-area: num;
+    width: 70%;
+    justify-self: right;
+  }
+  #complement {
+    grid-area: complement;
+    width: 90%;
+  }
+  #bairro {
+    grid-area: bairro;
+    width: 110%;
+    justify-self: right;
+  }
+  #city {
+    grid-area: city;
+    width: 130%;
+  }
+  #state {
+    grid-area: state;
+    width: 70%;
+    justify-self: right;
+  }
+  #button_finish {
+    grid-area: finish;
+    width: 70%;
+    justify-self: right;
+  }
+}
+
+.diferent-button {
+  background-color: var(--color-gray);
+  color: var(--color-black);
+
+  padding: 0.5rem 1rem;
+
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+
+  font-size: 1rem;
+  font-weight: bold;
+  background-color: var(--color-purple);
+  color: var(--color-white);
+}
 </style>
